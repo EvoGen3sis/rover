02dd115fb2588a3979a16085bde897ffa8ef9829
EvoGen3sis <192278290+EvoGen3sis@users.noreply.github.com>
2025-06-05 14:46:05 +0100

    Commit.

--- src/scripts/methods.js
+++ src/scripts/methods.js
@@ -2,6 +2,20 @@
 
 let directions = ["forwards", "left", "right", "backwards", ]
 
-function fetchAPI() {
-    
+/* function fetchAPI() {
+    fetch("/motor", {
+
+    })
+    .then(re)
+    .then()
+    .catch()
 }
+*/
+
+async function fetcher() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = response.json();
+    console.log(data);
+}
+
+fetcher();
